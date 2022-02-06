5ddf0a703574383734f5207b2ed4821b8eb4c6d3
eun-chae-s <s9710173@naver.com>
2022-02-06 00:48:41 -0500

    Hide firebase key

--- Buddy-Application/buddymain/src/firebase/firebase.js
+++ Buddy-Application/buddymain/src/firebase/firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCB_g-QSwAAlgXixXNwQOds0Hn02iTqir8",
-  authDomain: "buddy-8d12c.firebaseapp.com",
-  projectId: "buddy-8d12c",
-  storageBucket: "buddy-8d12c.appspot.com",
-  messagingSenderId: "138202497032",
-  appId: "1:138202497032:web:29c8706822e39a4812d1bd",
-  measurementId: "G-ECZRW42Y8L"
+
 };
 
 // Initialize Firebase
